15593c9c797b541e9125343da920526a17625150
michaeljguarino <mguarino46@gmail.com>
2023-11-13 15:40:44 -0500

    fix small syntax issue

--- app/main.py
+++ app/main.py
@@ -7,6 +7,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {
-      "Commit": os.environ.get('GIT_COMMIT')
+      "Commit": os.environ.get('GIT_COMMIT'),
       "From": os.environ.get('ENV', 'DEFAULT_ENV'),
     }
