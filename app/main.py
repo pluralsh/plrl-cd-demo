64800ee045f6faa30bc2cdc1ec1147606d327107
Michael Guarino <mjg@plural.sh>
2025-04-02 12:38:46 +0000

    Fix /ping endpoint to prevent raised exceptions leading to 500 Internal Server Errors

--- app/main.py
+++ app/main.py
@@ -12,8 +12,9 @@ prom.start_http_server(9090)
 
 @app.get("/ping")
 def test():
-  if int(time.time()) % 3 == 0:
-    raise Exception("unknown internal error")
+    # The following condition has been removed to prevent 500 errors:
+    # if int(time.time()) % 3 == 0:
+    #     raise Exception("unknown internal error")
 
   return {"pong": True}
 
