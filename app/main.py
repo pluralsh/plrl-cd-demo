40db08c76a45270279d1bba2e695b05a2a4bbfb4
Michael Guarino <mjg@plural.sh>
2025-06-30 16:06:41 +0000

    fix: remove intentional exception in /ping endpoint to prevent 500 errors

--- app/main.py
+++ app/main.py
@@ -12,9 +12,7 @@ prom.start_http_server(9090)
 
 @app.get("/ping")
 def test():
-  if int(time.time()) % 3 == 0:
-    raise Exception("unknown internal error")
-
+  # Removed intentional exception causing 500 errors.
   return {"pong": True}
 
 @app.get("/hello")
