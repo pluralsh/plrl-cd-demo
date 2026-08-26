c9b5939944ef5d5f98dad8a302e9e3a7f4899b8a
Aaron Smallberg <aaron@plural.sh>
2026-08-26 19:15:43 +0000

    fix: make ping endpoint consistently successful

--- app/test_main.py
+++ app/test_main.py
@@ -7,3 +7,19 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_ping_returns_pong():
+    response = client.get("/ping")
+
+    assert response.status_code == 200
+    assert response.json() == {"pong": True}
+
+
+def test_ping_returns_pong_when_time_is_divisible_by_three(monkeypatch):
+    monkeypatch.setattr("time.time", lambda: 3)
+
+    response = client.get("/ping")
+
+    assert response.status_code == 200
+    assert response.json() == {"pong": True}
