af48fd92ed66dc6daa760f1bc7a7d1f1889cb0fe
Aaron Smallberg <aaron@plural.sh>
2026-08-27 16:38:47 +0000

    fix: make ping endpoint consistently successful

--- app/test_main.py
+++ app/test_main.py
@@ -7,3 +7,10 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_ping_is_reliably_successful():
+    for _ in range(9):
+        response = client.get("/ping")
+        assert response.status_code == 200
+        assert response.json() == {"pong": True}
